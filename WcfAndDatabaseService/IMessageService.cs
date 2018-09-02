c5a7735056482ab0ebc12e3a65a3f9b5e5841e4d
Shaohua He <shaw@shaohuas-mbp.attlocal.net>
2018-09-01 21:13:51 -0400

    init

---
+++ WcfAndDatabaseService/IMessageService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfAndDatabaseService
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IService1”。
+    [ServiceContract(SessionMode = SessionMode.Required, CallbackContract = typeof(IWriteMessageToDatabaseCallback))]
+    public interface IMessageService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: 在此添加您的服务操作
+        [OperationContract(IsInitiating =true, IsTerminating = false)]
+        void WriteMessageToDatabase(string logMsg);
+
+        [OperationContract(IsInitiating =true, IsTerminating = false)]
+        void RegisterListener();
+
+        [OperationContract(IsInitiating =false, IsTerminating = false)]
+        void UnregisterListener();
+    }
+
+    [ServiceContract]
+    public interface IWriteMessageToDatabaseCallback 
+    {
+        [OperationContract(IsOneWay = true)]
+        void OnWriteMessageToDatabase(string logMsg);  
+    } 
+
+    // 使用下面示例中说明的数据约定将复合类型添加到服务操作。
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfAndDatabaseService/MessageService.svc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Diagnostics;
+
+namespace WcfAndDatabaseService
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service1”。
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true, InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode =ConcurrencyMode.Multiple)]
+    public class MessageService : IMessageService
+    {
+        public MessageService() 
+        { 
+            Trace.WriteLine("Create LogService Instance."); 
+        }
+
+        Dictionary<string, OperationContext> listeners = new Dictionary<string, OperationContext>();
+
+        private void BroadCast(string logMsg)
+        {
+            List<string> errorClints = new List<string>(); 
+            foreach (KeyValuePair<string, OperationContext>listener in listeners)
+            {
+                try
+                {
+                    listener.Value.GetCallbackChannel<IWriteMessageToDatabaseCallback>().OnWriteMessageToDatabase(logMsg);
+                }
+                catch (System.Exception e) 
+                { 
+                    errorClints.Add(listener.Key); 
+                    Trace.WriteLine("BROAD EVENT ERROR:" + e.Message); 
+                }
+            } 
+            foreach (string id in errorClints) 
+            { 
+                listeners.Remove(id); 
+            }
+        }
+
+        #region IMessageService 成员
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public void WriteMessageToDatabase(string logMsg)  
+        {  
+            Trace.WriteLine("Write LOG:"+logMsg);
+            BroadCast(logMsg);
+        }
+
+        public void RegisterListener()
+        {
+            listeners.Add(OperationContext.Current.SessionId, OperationContext.Current);
+            Trace.WriteLine("SessionID:" + OperationContext.Current.SessionId);
+            Trace.WriteLine("Register listener.Client Count:" + listeners.Count.ToString());
+        }
+        
+        public void UnregisterListener()  
+        {  
+            listeners.Remove(OperationContext.Current.SessionId); 
+            Trace.WriteLine("SessionID:" + OperationContext.Current.SessionId);  
+            Trace.WriteLine("Unregister listener. Client Count:" + listeners.Count.ToString());  
+        }  
+        #endregion
+    }
+}
